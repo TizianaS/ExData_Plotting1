3fd35cd2b70d772cf38e14af05681dd82659e06b
Tiziana S <tscarna@ulb.ac.be>
2015-06-04 13:07:52 +0100

    third commit

--- plot3.R
+++ plot3.R
@@ -18,10 +18,10 @@ y3<-Dates2days$Sub_metering_3
 ##Construct plot
 plot(x,y1,type="l",xlab="",ylab="Energy sub metering")
 lines(x,y2,col="red");lines(x,y3,col="blue")
-legend("topright", c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lty=c(1,1), lwd=c(2.5,2.5),col=c("black","blue","red")) 
+legend("topright", c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lty=c(1,1),col=c("black","blue","red")) 
 ##Construct plot and save it on png file with right size
 png(file = "plot3.png",width = 480, height = 480, units = "px")
 plot(x,y1,type="l",xlab="",ylab="Energy sub metering")
 lines(x,y2,col="red");lines(x,y3,col="blue")
-legend("topright", c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lty=c(1,1), lwd=c(2.5,2.5),col=c("black","blue","red")) 
+legend("topright", c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lty=c(1,1), col=c("black","blue","red")) 
 dev.off()
